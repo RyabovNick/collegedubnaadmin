737d4cd84c0a3a8f7d323b69ad23bd0ed0815d24
Nick <nikitos.aka.zed@gmail.com>
2018-07-23 11:07:08 +0300

    apiHelper added. Not sure it's right way. Need to check

--- routes/api/apiHelper.js
+++ routes/api/apiHelper.js
@@ -1,71 +1,104 @@
 /**
  * API for news
  */
-var pool = require('../../config/config');
+var pool = require('../../config/config'),
+    async = require('async');
 
 /**
  * Select all from table
+ * @param {response} res - send response from API
  * @param {string} table - table name
  */
-var findAll = function(tablename) {
+var findAll = function(res, tablename) {
     pool.getConnection(function(err, con) {
-        //console.log(tablename);
         if (err) throw err;
-        con.query('Select * from `news`', function(error, result) {
+        con.query('Select * from ??', [tablename], function(error, result) {
             if (error) throw error;
             if (result.length == 0) {
-                return sendStatus(401);
+                con.release();
+                return res.sendStatus(401);
             } else {
-                console.log('______________RESULT____________');
-                return result;
+                con.release();
+                return res.send(result);
             }
-            con.release();
         });
     });
 };
 
-module.exports.findAll = findAll; /*
-module.exports = function findById(table, id) {
+/**
+ * Select all rows from table for current id
+ * @param {response} res - send response from API
+ * @param {string} table - table name
+ * @param {int} id - value
+ */
+var findById = function findById(res, table, id) {
     pool.getConnection(function(err, con) {
         if (err) throw err;
         con.query('Select * from ?? where id = ?', [table, id], function(error, result) {
             if (error) throw error;
             if (result.length == 0) {
+                con.release();
                 return res.sendStatus(401);
             } else {
+                con.release();
                 return res.send(result);
             }
-            con.release();
         });
     });
 };
-*/ /*
-module.exports = function findByField(table, field, value) {
+
+/**
+ * Select all rows from table with where expression
+ * @param {response} res - send response from API
+ * @param {string} table - table name
+ * @param {string} field - what field are you looking for
+ * @param {string, int} value - the field value
+ */
+var findByField = function(res, table, field, value) {
     pool.getConnection(function(err, con) {
         if (err) throw err;
         con.query('Select * from ?? where ? = ?', [table, field, value], function(error, result) {
             if (error) throw error;
             if (result.length == 0) {
+                con.release();
                 return res.sendStatus(401);
             } else {
+                con.release();
                 return res.send(result);
             }
-            con.release();
         });
     });
 };
 
-module.exports = findAll;
-*/
+/**
+ * Search API
+ * @param {response} res - send response from API
+ * @param {string} table - table name
+ * @param {string} field - what field are you looking for
+ * @param {string, int} value - the field value
+ */
+var findByLike = function(res, table, field, value) {
+    pool.getConnection(function(err, con) {
+        if (err) throw err;
+        con.query("Select * from ?? where ? like '%?%'", [table, field, value], function(
+            error,
+            result
+        ) {
+            if (error) throw error;
+            if (result.length == 0) {
+                con.release();
+                return res.sendStatus(401);
+            } else {
+                con.release();
+                return res.send(result);
+            }
+        });
+    });
+};
 
-/**
- * Select all rows from table for current id
- * @param {string} table - table name
- * @param {int} id - value
- */
-/**
- * Select all rows from table with where expression
- * @param {string} table - table name
- * @param {*} field - what field are you looking for
- * @param {*} value - the field value
- */
+module.exports = {
+    findAll: findAll,
+    findById: findById,
+    findByField: findByField,
+    findByLike: findByLike,
+};
--- routes/api/news.js
+++ routes/api/news.js
@@ -5,23 +5,13 @@ var router = require('express').Router(),
     apiHelper = require('./apiHelper'),
     pool = require('../../config/config');
 
+/**
+ * apiHelper get response, tablename
+ * and send response to server
+ * Is it rigth? Fucking doubt, need to do it right
+ */
 router.get('/news', function(req, res, next) {
-    //console.log(typeof apiHelper.findAll('news'));
-    console.log('test');
-    console.log(apiHelper.findAll('news'));
-    return res.send('test');
-    /*pool.getConnection(function(err, con) {
-        if (err) throw err;
-        con.query('Select * from `news`', function(error, result) {
-            if (error) throw error;
-            if (result.length == 0) {
-                return res.sendStatus(401);
-            } else {
-                return res.send(result);
-            }
-            con.release();
-        });
-    });*/
+    apiHelper.findAll(res, 'news');
 });
 
-(module.exports = router), apiHelper;
+module.exports = router;
