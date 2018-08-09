fe18a118f10ff1e71e6e84ef794e6b01a574e50f
Nick <nikitos.aka.zed@gmail.com>
2018-08-09 11:37:59 +0300

    education completed

--- routes/api/admin/education.js
+++ routes/api/admin/education.js
@@ -13,7 +13,45 @@ var router = require('express').Router(),
 router
     .route('/admin/education/eduaccred')
     .post(auth.required, function(req, res, next) {
-        apiHelper.insert(res, 'eduaccred', req.body);
+        pool.getConnection(function(err, con) {
+            if (err) throw err;
+            con.query(
+                'INSERT INTO `eduaccred` (`eduCode`, `eduName`, `eduLevel`, `eduForm`, `learningTerm`, `language`, `dateEnd`)' +
+                    ' VALUES (?,?,?,?,?,?,?)',
+                [
+                    req.body.eduCode,
+                    req.body.eduName,
+                    req.body.eduLevel,
+                    req.body.eduForm,
+                    req.body.learningTerm,
+                    req.body.language,
+                    req.body.dateEnd,
+                ],
+                function(error, result) {
+                    if (error) return res.status(400).send(error);
+                    if (result.length == 0) {
+                        con.release();
+                        return res.sendStatus(204);
+                    } else {
+                        req.body.years.forEach((year) => {
+                            con.query(
+                                'Insert INTO `eduop` (`id_eduCode`, `year`) VALUE (?,?)',
+                                [result.insertId, year],
+                                function(error, result) {
+                                    if (error) return res.status(400).send(error);
+                                    if (result.length == 0) {
+                                        con.release();
+                                        return res.sendStatus(204);
+                                    }
+                                }
+                            );
+                        });
+                        con.release();
+                        return res.send(result);
+                    }
+                }
+            );
+        });
     })
     .put(auth.required, function(req, res, next) {
         apiHelper.update(res, 'eduaccred', req.body);
