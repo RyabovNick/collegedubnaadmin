08173c6b62b20932d66bf88f4a4064b46f969c26
Ryabov Nikita <nikitos.aka.zed@gmail.com>
2019-02-12 17:57:31 +0300

    cabinets and educode api added

--- routes/api/objects.js
+++ routes/api/objects.js
@@ -3,6 +3,7 @@
  */
 const router = require('express').Router();
 const apiHelper = require('./apiHelper');
+const pool = require('../../config/config');
 
 /**
  * get all from common
@@ -23,6 +24,48 @@ router.get('/purposelibr', function(req, res, next) {
  */
 router.get('/purposeeios', function(req, res, next) {
     apiHelper.findAll(res, 'purposeeios');
+});
+
+/**
+ * get all eduCode
+ */
+router.get('/educode', function(req, res, next) {
+    pool.query(
+        'SELECT distinct edua.id, concat(edua.eduCode, " ", edua.eduName) as cab_head ' +
+            'FROM eduaccred_cabinets ec ' +
+            'INNER JOIN eduaccred edua on edua.id = ec.edu_id',
+        (error, result) => {
+            if (error) return res.status(400).send(error);
+            if (result.length == 0) {
+                return res.sendStatus(204);
+            } else {
+                return res.send(result);
+            }
+        }
+    );
+});
+
+/**
+ * get all cabinets for id eduCode (eduAccred)
+ */
+router.get('/cabinets/:id', function(req, res, next) {
+    const id = req.params['id'];
+    pool.query(
+        'SELECT edca.name as discipline, cab.name as cabname, cab.address, cab.disabled ' +
+            'FROM eduaccred edua ' +
+            'INNER JOIN eduaccred_cabinets edca on edua.id = edca.edu_id ' +
+            'INNER JOIN cabinets cab on cab.id = edca.cabinet_id ' +
+            'where edua.id = ?',
+        [id],
+        (error, result) => {
+            if (error) return res.status(400).send(error);
+            if (result.length == 0) {
+                return res.sendStatus(204);
+            } else {
+                return res.send(result);
+            }
+        }
+    );
 });
 
 module.exports = router;
